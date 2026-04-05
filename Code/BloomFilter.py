8bfb1043b196e63ef8103319f877d23f042c6d5e
Arnoud Vandevelde <arnoud.vandevelde@gmail.com>
2026-04-05 11:16:24 +0200

    Added 'add' method to Bloom filter class.
    The add method adds a given string x to the Bloom filter.

--- Code/BloomFilter.py
+++ Code/BloomFilter.py
@@ -4,3 +4,9 @@ class BloomFilter:
     def __init__(self, m: int, *hash_functions) -> None:
         self.m = m
         self.hash_functions = hash_functions
+        self.filter = m*[0]
+
+    def add(self, x: str) -> None:
+        """adds the given string x to the Bloom filter."""
+        for hash_function in self.hash_functions:
+            self.filter[hash_function(x) % self.m] = 1
