90110cc394aafccf55bd24e36b1138d1bcdbebb7
Arnoud Vandevelde <arnoud.vandevelde@gmail.com>
2026-04-08 12:49:03 +0200

    Add examples of search method of Bloom filter
    Added examples of search including elements not part of bloom filter and false positives.

--- examples/adding_and_searching.py
+++ examples/adding_and_searching.py
@@ -4,9 +4,18 @@ from src.BloomFilter import BloomFilter
 m = 5
 small_filter = BloomFilter(m, djb2)
 
+# Adding elements
 print(djb2("a") % m)
 print(djb2("b") % m)
-
 small_filter.add("a")
 small_filter.add("b")
 print(small_filter.filter)
+
+
+# Searching elements
+print(small_filter.search("a"))
+print(small_filter.search("b"))
+print(djb2("c") % m)
+print(small_filter.search("c"))  # Not in set
+print(djb2("z") % m)
+print(small_filter.search("z"))  # False positive
